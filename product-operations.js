56013b9d3f62235087b45292d254ebd638321692
Ishanra03 <148366944+Ishanra03@users.noreply.github.com>
2025-07-03 15:06:53 +0530

    Update product-operations.js

--- product-operations.js
+++ product-operations.js
@@ -3,6 +3,17 @@ import Product from "../MODELS/product.js";
 import doNetworkCall from "../api-client.js";
 
 const productOperations={
+    products:[],    //Key-Value
+    search(pizzaId){
+       const product= this.products.find(currentProduct=>currentProduct.id==pizzaId);
+       console.log('Product found:', product);
+       product.isAddedInCart=true;
+       console.log('Array',this.products);
+    },
+    getProductsInCart(){
+       const productInBasket= this.products.filter(product=>product.isAddedInCart);
+       return productInBasket;
+    },
     async loadProducts(){
    const pizzas = await doNetworkCall();
    const pizzaArray = pizzas['Vegetarian'];
@@ -14,7 +25,8 @@ const productOperations={
         pizza.URL);
     return curentPizza;
    });
-   console.log('Product array ',productsArray);
+   console.log('***Product array ',productsArray);
+   this.products=productsArray;
    return productsArray;  
     },
     sortProducts(){
