011d581cac2936567020fd291068a2cb81ee0fd1
Ishanra03 <148366944+Ishanra03@users.noreply.github.com>
2025-07-16 13:06:16 +0530

    Update product.js

--- product.js
+++ product.js
@@ -1,14 +1,14 @@
-//Product Model(Blue Print)
+// scripts/MODELS/product.js
 
-class Product{
-constructor(id,name,desc,price,url){
-    this.id=id;
-    this.name=name;
-    this.desc=desc;
-    this.price=price;
-    this.URL=URL;
-    this.isAddedInCart=false;
-    
+class Product {
+    constructor(id, name, desc, price, url) {
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+        this.price = price;
+        this.url = url;
+        this.isAddedInCart = false;  // ✅ Default value
+    }
 }
-}
+
 export default Product;
